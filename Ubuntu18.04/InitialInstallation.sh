86740767237a1dc523e8e814856f7d1b153ee397
SerraZ3 <serra.henrique3@gmail.com>
2019-12-10 21:56:03 -0300

    FiraCode adicionada

--- Ubuntu18.04/InitialInstallation.sh
+++ Ubuntu18.04/InitialInstallation.sh
@@ -292,3 +292,31 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     sudo apt-get install pgmodeler -y
     echo "Para roda rode no terminal 'pgmodeler'"
 fi
+
+
+echo -n "Instalar Fonte Firacode(y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Instalando Firacode"
+    fonts_dir="${HOME}/.local/share/fonts"
+    if [ ! -d "${fonts_dir}" ]; then
+        echo "mkdir -p $fonts_dir"
+        mkdir -p "${fonts_dir}"
+    else
+        echo "Found fonts dir $fonts_dir"
+    fi
+
+    for type in Bold Light Medium Regular Retina; do
+        file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+        file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+        if [ ! -e "${file_path}" ]; then
+            echo "wget -O $file_path $file_url"
+            wget -O "${file_path}" "${file_url}"
+        else
+        echo "Found existing file $file_path"
+        fi;
+    done
+
+    echo "fc-cache -f"
+    fc-cache -f
+fi
