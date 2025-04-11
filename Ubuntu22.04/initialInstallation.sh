a9097a6817821e0b3b5b0416b3fd9b44ae35682f
SerraZ <30788371+SerraZ3@users.noreply.github.com>
2025-04-11 08:48:08 -0300

    firacode

--- Ubuntu22.04/initialInstallation.sh
+++ Ubuntu22.04/initialInstallation.sh
@@ -180,29 +180,7 @@ fi
 echo -n "Fonte Firacode(y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
-  echo "Installing..."
-  fonts_dir="${HOME}/.local/share/fonts"
-  if [ ! -d "${fonts_dir}" ]; then
-    echo "mkdir -p $fonts_dir"
-    mkdir -p "${fonts_dir}"
-  else
-    echo "Found fonts dir $fonts_dir"
-  fi
-
-  for type in Bold Light Medium Regular Retina; do
-    file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
-    file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
-    if [ ! -e "${file_path}" ]; then
-      echo "wget -O $file_path $file_url"
-      wget -O "${file_path}" "${file_url}"
-    else
-      echo "Found existing file $sudo apt update
-file_path"
-    fi
-  done
-
-  echo "fc-cache -f"
-  fc-cache -f
+ sudo apt install fonts-firacode
 fi
 
 echo -n "NVM(y/n)? "
